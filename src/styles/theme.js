6a781408f669d4588aafe16e79671128f844d0a6
Philippe Candille <philippe@oclock.io>
2020-02-11 09:39:57 +0100

    theme ok

---
+++ src/styles/theme.js
@@ -0,0 +1,14 @@
+import tinycolor from 'tinycolor2';
+
+// Permet de générer une couleur plus claire
+// color : couleur à traiter et percent: pourcentage à appliquer
+export const lighten = (color, percent) => tinycolor(color).lighten(percent).toString();
+// Permet de générer une couleur plus foncée
+// color : couleur à traiter et percent: pourcentage à appliquer
+export const darken = (color, percent) => tinycolor(color).darken(percent).toString();
+
+const theme = {
+  mainColor: '#0457A0',
+};
+
+export default theme;
