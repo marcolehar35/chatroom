adc38eda2a923eaaeb799c0bb6542d50b5e7a157
marcolehar35 <lehartel.com@gmail.com>
2020-02-11 09:23:28 +0100

    structure de base ok

---
+++ src/components/Settings/index.js
@@ -0,0 +1,14 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+
+// == Composant
+const Settings = () => (
+  <div>
+    Settings
+  </div>
+);
+
+// == Export
+export default Settings;
