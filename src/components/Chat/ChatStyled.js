be9fe3f862da9e546e7dd13ebe04f8a1bb0c3c46
marcolehar35 <lehartel.com@gmail.com>
2020-02-11 10:57:17 +0100

    Chat fix width

--- src/components/Chat/ChatStyled.js
+++ src/components/Chat/ChatStyled.js
@@ -4,7 +4,9 @@ import theme, { lighten } from 'src/styles/theme';
 
 const ChatStyled = styled.div`
   max-width: 700px;
-  margin: 0 auto 0 2em;
+  min-width: 400px;
+  width: 60%;
+  margin: 0 auto 0 5em;
   background-color: ${lighten(theme.mainColor, 60)};
   height: 100vh;
   box-shadow: 0 0 3em rgba(0, 0, 0, .4);
