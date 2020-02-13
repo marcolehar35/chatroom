acc3b2a022aec9c4c2e4be29dd86e88c4c84c650
marcolehar35 <lehartel.com@gmail.com>
2020-02-13 10:02:42 +0100

    send message

--- src/actions/chat.js
+++ src/actions/chat.js
@@ -1,8 +1,13 @@
 // Types
 export const CHANGE_MESSAGE_INPUT = 'CHANGE_MESSAGE_INPUT';
+export const SEND_NEW_MESSAGE = 'SEND_NEW_MESSAGE';
 
 // Creators
 export const changeMessageInput = (value) => ({
   type: CHANGE_MESSAGE_INPUT,
   value,
+});
+
+export const sendNewMessage = () => ({
+  type: SEND_NEW_MESSAGE,
 });
--- src/reducers/chat.js
+++ src/reducers/chat.js
@@ -1,5 +1,8 @@
 /* eslint-disable no-console */
-import { CHANGE_MESSAGE_INPUT } from 'src/actions/chat';
+import {
+  CHANGE_MESSAGE_INPUT,
+  SEND_NEW_MESSAGE,
+} from 'src/actions/chat';
 
 // Initial State
 const initialState = {
@@ -36,6 +39,25 @@ const chatReducer = (state = initialState, action = {}) => {
         ...state, // state actuel
         inputMessage: action.value,
       };
+
+    case SEND_NEW_MESSAGE: {
+      // temporaire : on va améliorer ça :)
+      const messageId = Math.max(...state.messages.map((message) => message.id)) + 1;
+
+      const newMessage = {
+        id: messageId,
+        content: state.inputMessage,
+        author: state.username,
+      };
+
+      return {
+        ...state,
+        // messages sera un nouveau tableau (immutabilité) avec le contenu de l'ancien
+        // + le nouveau message
+        messages: [...state.messages, newMessage],
+        inputMessage: '',
+      };
+    }
 
     // entré dans aucune action : je retourne le state sans modif
     default:
