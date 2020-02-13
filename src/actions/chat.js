f80d1a30054605ca67d461e8777b80b36266b002
marcolehar35 <lehartel.com@gmail.com>
2020-02-13 15:23:01 +0100

    authMiddleware

--- src/actions/chat.js
+++ src/actions/chat.js
@@ -3,6 +3,7 @@ export const CHANGE_MESSAGE_INPUT = 'CHANGE_MESSAGE_INPUT';
 export const SEND_NEW_MESSAGE = 'SEND_NEW_MESSAGE';
 export const CHANGE_FIELD = 'CHANGE_FIELD';
 export const LOGIN = 'LOGIN';
+export const SAVE_USERNAME = 'SAVE_USERNAME';
 
 // Creators
 export const changeMessageInput = (value) => ({
@@ -22,4 +23,9 @@ export const changeField = (name, value) => ({
 
 export const login = () => ({
   type: LOGIN,
+});
+
+export const saveUsername = (username) => ({
+  type: SAVE_USERNAME,
+  username,
 });
